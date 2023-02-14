eda5666703e33556bd203b0391b488ddeab49ada
dreyman_pa <dreyman_pa@ic-iskra.ru>
2023-02-14 12:37:08 +0700

    Xrandr config Two-mon v13-02-2023

---
+++ xrandr/xrandr.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Change SCREEN on Two monitors 
+#
+
+montype=~/Screen_types_xrandr
+MONTYPE='(DVI|VGA|HDMI|DP)'
+
+MONITOR1=(VGA-1)
+MONITOR2=(DVI-I-1)
+
+COMM=(/usr/bin/xrandr)
+${COMMAND} --prop | xargs -0 | egrep $MONTYPE  > $montype
+
+
+${COMMAND} --output ${MONITOR1} --left-of ${MONITOR2}
+#${COMMAND} --output ${MONITOR1} --right-of ${MONITOR2}
+
+${COMMAND} --output $MONITOR1 --primary
